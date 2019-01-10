9530de64de9b21d22104c6efd29456a332b905ff
Angelo James Arriola <angelojames.arriola@gmail.com>
2019-01-09 20:24:09 -0800

    create a button on press makes a GET request and maps data onto view

--- App.js
+++ App.js
@@ -1,32 +1,20 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { ScrollView } from 'react-native';
 import UserProfile from './src/components/UserProfile';
 import { Provider } from 'react-redux';
+
 import store from './store';
 
 class App extends Component {
   render() {
     return (
       <Provider store={store}>
-        <View style={styles.container}>
-          <Text>Welcome to Runners Mind</Text>
-          <Button 
-            title="Get Data"
-          />
+        <ScrollView>
           <UserProfile />
-        </View>
+        </ScrollView>
       </Provider>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
 
 export default App;
--- src/components/UserProfile.js
+++ src/components/UserProfile.js
@@ -1,21 +1,51 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { View, Text } from 'react-native';
+import { View, Text, Button, StyleSheet } from 'react-native';
+import { bindActionCreators } from 'redux';
+import { getUser } from '../actions/userActions';
+import UsersList from './UsersList';
 
 class UserProfile extends Component {
   render() {
     return (
-      <View>
-        <Text>{this.props.email}</Text>
-      </View>
+      <View style={styles.container}>
+        <Text style={styles.runnersText}>Welcome to Runners Mind</Text>
+        <Button 
+          onPress={()=>{this.props.getUser()}}
+          title="Get Data"
+        />
+        <UsersList />
+      </View> 
     )
   }
 }
 
-const mapStateToProps = state => {
+const mapDispatchToProps = (dispatch => {
+  return {
+    getUser: bindActionCreators(getUser, dispatch)
+  }
+})
+
+const mapStateToProps = (state, dispatch) => {
   return {
     ...state.user
   }
 }
 
-export default connect(mapStateToProps, null)(UserProfile);
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#fff',
+    width: '100%',
+    // alignItems: 'center',
+    // justifyContent: 'center',
+    marginTop: 40,
+    flexDirection: 'column',
+    justifyContent: 'center'
+    
+  },
+  runnersText: {
+    textAlign: 'center'
+  }
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(UserProfile);
